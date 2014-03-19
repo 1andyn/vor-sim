dda57fe14c9105d2909c8af53ba567d754b7abde
andyng <andyng@hawaii.edu>
2014-03-18 14:53:34 -1000

    basic label testing
    Signed-off-by: andyng <andyng@hawaii.edu>

--- src/ics314/Interface.java
+++ src/ics314/Interface.java
@@ -1,22 +1,33 @@
 package ics314;
 
+import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Shell;
 
 public class Interface {
+		
+	
 	public static void main(String[] args) {
-		Display iface_Disp = new Display();
-		Shell iface_Shell = new Shell(iface_Disp);
+		Display Disp = new Display();
+		Shell Disp_Shell = new Shell(Disp);
 		
-		iface_Shell.setLayout(new FillLayout());
-		iface_Shell.open();
+		Label PlaneCoordLabel = new Label(Disp_Shell, SWT.BORDER);
+		PlaneCoordLabel.setText("Plane Coordinates (x,y): ");
+		Label PlaneAngleLabel = new Label(Disp_Shell, SWT.BORDER);
+		PlaneAngleLabel.setText("Plane Angle (Degrees): ");
+		Label OBSLabel = new Label(Disp_Shell, SWT.BORDER);
+		OBSLabel.setText("OBS Knob Angle (Degrees): ");
 		
-		while(!iface_Shell.isDisposed()) {
-			if(!iface_Disp.readAndDispatch()) {
-				iface_Disp.sleep();
+		Disp_Shell.setLayout(new FillLayout());
+		Disp_Shell.open();
+		
+		while(!Disp_Shell.isDisposed()) {
+			if(!Disp.readAndDispatch()) {
+				Disp.sleep();
 			}
 		}
-		iface_Disp.dispose();
+		Disp.dispose();
 	}
 }
