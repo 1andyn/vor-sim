509f15a2b01f9c24a71f124607fce5e51127613c
andyng <andyng@hawaii.edu>
2014-03-18 14:39:53 -1000

    added readme file, have basic SWT window working
    Signed-off-by: andyng <andyng@hawaii.edu>

--- src/ics314/Interface.java
+++ src/ics314/Interface.java
@@ -1,5 +1,22 @@
 package ics314;
 
-public class Interface {
+import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
 
+public class Interface {
+	public static void main(String[] args) {
+		Display iface_Disp = new Display();
+		Shell iface_Shell = new Shell(iface_Disp);
+		
+		iface_Shell.setLayout(new FillLayout());
+		iface_Shell.open();
+		
+		while(!iface_Shell.isDisposed()) {
+			if(!iface_Disp.readAndDispatch()) {
+				iface_Disp.sleep();
+			}
+		}
+		iface_Disp.dispose();
+	}
 }
