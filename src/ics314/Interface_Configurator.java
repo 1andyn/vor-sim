1d173bc75db87764e249977179f7c9475797d1bb
andyng <andyng@hawaii.edu>
2014-03-22 23:53:23 -1000

    rewrote interface code to be cleaner and use interfaces

---
+++ src/ics314/Interface_Configurator.java
@@ -0,0 +1,22 @@
+package ics314;
+
+import org.eclipse.swt.widgets.Spinner;
+
+public class Interface_Configurator {
+	
+	public void SpinnerCoordConfig (Spinner input_spin){
+		input_spin.setMinimum(Interface_Const.MINCOORD);
+	    input_spin.setMaximum(Interface_Const.MAXCOORD);
+	    input_spin.setSelection(Interface_Const.START_COORD);
+	    input_spin.setIncrement(Interface_Const.COORD_INCREMENT);
+	    input_spin.setPageIncrement(Interface_Const.COORD_PAGE_INCREMENT);
+	}
+	
+	public void SpinnerAngleConfig (Spinner input_spin){
+		input_spin.setMinimum(Interface_Const.MINANGLE);
+		input_spin.setMaximum(Interface_Const.MAXANGLE);
+		input_spin.setSelection(Interface_Const.START_ANGLE);
+		input_spin.setIncrement(Interface_Const.ANGLE_INCR);
+		input_spin.setPageIncrement(Interface_Const.ANGLE_PAGEINCR);	
+	}
+}
---
+++ src/ics314/Interface_Const.java
@@ -0,0 +1,20 @@
+package ics314;
+
+public interface Interface_Const {
+	
+	static final int ColumnCount = 4;
+	static final int FillTwoColumns = 2;
+	static final int SingleColumn = 1;
+	static final int FillSingleRow = 1;
+	static final int MAXCOORD = 100;
+	static final int MINCOORD = -100;
+	static final int MINANGLE = 0;
+	static final int MAXANGLE = 360;
+	static final int START_COORD = 0;
+	static final int COORD_INCREMENT = 1;
+	static final int COORD_PAGE_INCREMENT = 10;
+	static final int START_ANGLE = 0;
+	static final int ANGLE_INCR = 1;
+	static final int ANGLE_PAGEINCR = 20;
+	
+}
