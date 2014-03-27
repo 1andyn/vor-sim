1a415774ebca5d51d55e40228e3e3a8fdf0d4d85
andyng <andyng@hawaii.edu>
2014-03-26 20:20:48 -1000

    adding code to radio

--- src/ics314/Radio.java
+++ src/ics314/Radio.java
@@ -1,5 +1,74 @@
 package ics314;
 
 public class Radio {
-
+	private int PLANE_X_COORD;
+	private int PLANE_Y_COORD;
+	private int PLANE_ANGLE;
+	private int OBS_ANGLE;
+	
+	public Radio(int x, int y, int p, int o)
+	{
+		PLANE_X_COORD = x;
+		PLANE_Y_COORD = y;
+		PLANE_ANGLE = p;
+		OBS_ANGLE = o;
+	}
+	
+	public Radio()
+	{
+		initRadio();
+	}
+	
+	private void initRadio()
+	{
+		PLANE_X_COORD = Interface_Const.START_COORD;
+		PLANE_Y_COORD = Interface_Const.START_COORD;
+		PLANE_ANGLE = Interface_Const.START_ANGLE;
+		OBS_ANGLE = Interface_Const.START_ANGLE;
+	}
+	
+	public void setPlaneAngle(int angle)
+	{
+		PLANE_ANGLE = angle;
+	}
+	
+	public void setOBSAngle(int angle)
+	{
+		OBS_ANGLE = angle;
+	}
+	
+	public void setXCoord(int x)
+	{
+		PLANE_X_COORD = x;
+	}
+	
+	public void setYCoord(int y)
+	{
+		PLANE_Y_COORD = y;
+	}
+	
+	public int getPlaneAngle()
+	{
+		int temp = PLANE_ANGLE;
+		return temp;
+	}
+	
+	public int getXCoord()
+	{
+		int temp = PLANE_X_COORD;
+		return temp;
+	}
+	
+	public int getYCoord()
+	{
+		int temp = PLANE_Y_COORD;
+		return temp;
+	}
+	
+	public int getOBSAngle()
+	{
+		int temp = OBS_ANGLE;
+		return temp;
+	}
+	
 }
