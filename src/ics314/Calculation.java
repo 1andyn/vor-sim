15c555a11c3ce9ce16832a70f95c87975282fa77
andyng <andyng@hawaii.edu>
2014-03-30 14:41:16 -1000

    commented out unfinished functions

--- src/ics314/Calculation.java
+++ src/ics314/Calculation.java
@@ -1,4 +1,23 @@
 package ics314;
 
 public class Calculation {
+	
+	private int PREV_COORD_X;
+	private int PREV_COORD_Y;
+	
+	public Calculation(){
+		
+	}
+	/*
+	public int calculateToFrom(int x, int y, int angle){
+		PREV_COORD_X = x;
+		PREV_COORD_Y = y;
+		
+	}
+	
+	public int calculateWestEast(int x, int y, int angle){
+		PREV_COORD_X = x;
+		PREV_COORD_Y = y;
+	}
+	*/
 }
