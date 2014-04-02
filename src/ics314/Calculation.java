15e912e89de4ee58b4f1372279b763efe16c526d
andyng <andyng@hawaii.edu>
2014-04-01 15:04:39 -1000

    added simple calculation code (untested)

--- src/ics314/Calculation.java
+++ src/ics314/Calculation.java
@@ -8,16 +8,30 @@ public class Calculation {
 	public Calculation(){
 		
 	}
-	/*
+	
 	public int calculateToFrom(int x, int y, int angle){
 		PREV_COORD_X = x;
-		PREV_COORD_Y = y;
+		PREV_COORD_Y= y;
+		
+		float cosine =(float)Math.cos(angle);
+		float sine = (float)Math.sin(angle);
+		
+		int new_x = (int)(x*cosine -y*sine);
+		if(x < new_x) {
+			return Interface_Const.WEST;
+		} else if (x > new_x) {
+			return Interface_Const.EAST;
+		} else {
+			return Interface_Const.UNCHANGED;
+		}
 		
 	}
 	
 	public int calculateWestEast(int x, int y, int angle){
-		PREV_COORD_X = x;
 		PREV_COORD_Y = y;
+		PREV_COORD_Y= y;
+		
+		return Interface_Const.UNCHANGED;
 	}
-	*/
+	
 }
