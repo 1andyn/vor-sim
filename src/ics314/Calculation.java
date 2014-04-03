dd54e8106986b8e6951f7a35f0bd65da0ebede8d
andyng <andyng@hawaii.edu>
2014-04-02 16:37:44 -1000

    added new member variables, new functions

--- src/ics314/Calculation.java
+++ src/ics314/Calculation.java
@@ -4,26 +4,34 @@ public class Calculation {
 	
 	private int PREV_COORD_X;
 	private int PREV_COORD_Y;
+	private int Quadrant;
 	
-	public Calculation(){
-		
+	public Calculation()
+	{
+		init_Calc();
 	}
 	
-	public int calculateToFrom(int x, int y, int angle){
+	private void init_Calc()
+	{
+		PREV_COORD_X = Interface_Const.p_cx;
+		PREV_COORD_Y = Interface_Const.p_cy;
+		Quadrant = Interface_Const.Q_CENTER;
+	}
+	
+	public int calculateToFrom(int x, int y, int angle)
+	{
 		PREV_COORD_X = x;
 		PREV_COORD_Y= y;
 		
-		float cosine =(float)Math.cos(angle);
-		float sine = (float)Math.sin(angle);
+		int rotated_x = getRotatedX(x,y,angle);
+		int rotated_y = getRotatedY(x,y,angle);
+
 		
-		int new_x = (int)(x*cosine -y*sine);
-		if(x < new_x) {
-			return Interface_Const.WEST;
-		} else if (x > new_x) {
-			return Interface_Const.EAST;
-		} else {
-			return Interface_Const.UNCHANGED;
-		}
+		
+		
+		
+		
+		return Interface_Const.UNCHANGED;
 		
 	}
 	
@@ -33,5 +41,41 @@ public class Calculation {
 		
 		return Interface_Const.UNCHANGED;
 	}
+	
+	private int calculateQuadrant(int r_x, int r_y, int angle)
+	{
+		if(angle < Interface_Const.HALF_PI || angle == Interface_Const.TWO_PI){
+			return rotatedByLessThanFourth(r_x, r_y, angle);
+		} else if (angle >= Interface_Const.HALF_PI && 
+				angle < Interface_Const.THREE_FORTH_PI) {
+			
+		}
+	}
+	
+	private int rotatedByLessThanFourth(int r_x, int r_y, int angle)
+	{
+		
+	}
+	private int rotatedByFourth()
+	private int rotatedByHalf()
+	private int rotatedByThreeFourth()
+	
+	private int getRotatedX(int x, int y, int angle)
+	{
+		float cosine =(float)Math.cos(angle);
+		float sine = (float)Math.sin(angle);
+		int new_x = (int)(x*cosine -y*sine);
+		return new_x;
+	}
+	
+	private int getRotatedY(int x, int y, int angle)
+	{
+		float cosine =(float)Math.cos(angle);
+		float sine = (float)Math.sin(angle);
+		int new_y = (int)(x*sine + y*cosine);
+		return new_y;
+	}
+	
+
 	
 }
