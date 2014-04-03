dac98516478dd74e3c33af8fa5ccf4d504a6038d
andyng <andyng@hawaii.edu>
2014-04-02 17:50:56 -1000

    updated old functions

--- src/ics314/Calculation.java
+++ src/ics314/Calculation.java
@@ -2,8 +2,8 @@ package ics314;
 
 public class Calculation {
 	
-	private int PLANE_COORD_X; // X Coordinate of Plane
-	private int PLANE_COORD_Y; // Y Coordinate of Plane
+	private int PLANE_COORD_X; // Normalized X Coordinate of Plane
+	private int PLANE_COORD_Y; // Normalized Y Coordinate of Plane
 	private int TOFROM;
 	private int WESTEAST;
 	
@@ -22,98 +22,52 @@ public class Calculation {
 	
 	public int calculateToFrom(int x, int y, int angle)
 	{
-		PLANE_COORD_X = x;
-		PLANE_COORD_Y= y;
+		PLANE_COORD_X = x - Interface_Const.p_cx;
+		PLANE_COORD_Y= y - Interface_Const.p_cy;
 		
-		int rotated_x = getRotatedX(x,y,angle);
-		int rotated_y = getRotatedY(x,y,angle);
+		int rotated_x = getRotatedX(angle);
+		int rotated_y = getRotatedY(angle);
 
-		updateQuadrant(rotated_x, rotated_y, angle);
+		updateOutputs(rotated_x, rotated_y, angle);
 		return TOFROM;
 	}
 	
 	public int calculateWestEast(int x, int y, int angle)
 	{
-		PLANE_COORD_X = x;
-		PLANE_COORD_Y= y;
+		PLANE_COORD_X = x - Interface_Const.p_cx;
+		PLANE_COORD_Y= y - Interface_Const.p_cy;
 		
-		int rotated_x = getRotatedX(x,y,angle);
-		int rotated_y = getRotatedY(x,y,angle);
+		int rotated_x = getRotatedX(angle);
+		int rotated_y = getRotatedY(angle);
 
-		updateQuadrant(rotated_x, rotated_y, angle);
+		updateOutputs(rotated_x, rotated_y, angle);
 		return WESTEAST;	
 	}
 	
-	private void updateQuadrant(int r_x, int r_y, int angle)
-	{
-		if(angle < Interface_Const.HALF_PI || angle == Interface_Const.TWO_PI ){
-			rotatedByLessThanFourth(r_x, r_y, angle);
-		} else if (angle >= Interface_Const.HALF_PI && 
-				angle < Interface_Const.PI) {
-			rotatedByFourth(r_x, r_y, angle);
-		} else if (angle >= Interface_Const.PI &&
-				angle < Interface_Const.THREE_FORTH_PI) {
-			rotatedByHalf(r_x, r_y, angle);
-		} else if (angle >= Interface_Const.THREE_FORTH_PI &&
-				angle < Interface_Const.TWO_PI) {
-			rotatedByThreeFourth(r_x, r_y, angle);
-		} else {
-			/* This should not occur ever */
-			System.out.println("Quadrant function failed.");
-		}
-	}
 	
-	/*	If Rotated by Less than 90 Deg, zero or greater
-			WESTEAST -> Compare R_X with X, TOFROM -> R_Y with Y
-		If Rotated by 90, less than 180,
-			WESTEAST -> Compare R_X with Y, TOFROM -> R_Y with X
-		If Rotated by 180, less than 270
-			WESTEAST -> Compare R_X with -X, TOFROM -> R_Y with -Y
-		If Rotated by 270, less than 360
-			WESTEAST -> Compare R_X with -Y, TOFROM -> R_Y with -X
-	*/
-	private void rotatedByLessThanFourth(int r_x, int r_y, int angle)
+	private void updateOutputs(int r_x, int r_y, int angle)
 	{
 		
-		if(r_x == PLANE_COORD_X) {
-			WESTEAST = Interface_Const.CENTER;
-		} else if (r_x > PLANE_COORD_X) {
-			WESTEAST= Interface_Const.
-		}
-		
-		
-		//if(PLANE_COORD_X > r_x )
+
+
 	}
-	private void rotatedByFourth(int r_x, int r_y, int angle)
-	{
-		
-	}
-	
-	private void rotatedByHalf(int r_x, int r_y, int angle)
-	{
-		
-	}
-	
-	private void rotatedByThreeFourth(int r_x, int r_y, int angle)
-	{
-		
-	}
+
 	
 	/* Angle is negative since negative angle rotated CLOCK WISE*/
 	
-	private int getRotatedX(int x, int y, int angle)
+	private int getRotatedX(int angle)
 	{
 		float cosine =(float)Math.cos(-angle);
 		float sine = (float)Math.sin(-angle);
-		int new_x = (int)(x*cosine -y*sine);
+		int new_x = (int)(PLANE_COORD_X*cosine -PLANE_COORD_Y*sine);
 		return new_x;
 	}
 	
-	private int getRotatedY(int x, int y, int angle)
+	private int getRotatedY(int angle)
 	{
 		float cosine =(float)Math.cos(-angle);
 		float sine = (float)Math.sin(-angle);
-		int new_y = (int)(x*sine + y*cosine);
+		int new_y = (int)(PLANE_COORD_X*sine + PLANE_COORD_Y*cosine);
 		return new_y;
 	}
 	
