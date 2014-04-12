fcb9d81300643ecb9d10bb1e69063a589961ead2
andyng <andyng@hawaii.edu>
2014-04-11 22:40:03 -1000

    deleted/commented out unused code

--- src/ics314/Calculation.java
+++ src/ics314/Calculation.java
@@ -2,10 +2,9 @@ package ics314;
 
 public class Calculation {
 	
-	private int PLANE_COORD_X; // Normalized X Coordinate of Plane
-	private int PLANE_COORD_Y; // Normalized Y Coordinate of Plane
 	private int TOFROM;
-	private int WESTEAST;
+	private int OBS_ANGLE;
+	private int RADIO_DEFLECTION;
 	
 	public Calculation()
 	{
@@ -14,72 +13,63 @@ public class Calculation {
 	
 	private void init_Calc()
 	{
-		PLANE_COORD_X = Iface_Const.p_cx;
-		PLANE_COORD_Y = Iface_Const.p_cy;
-		TOFROM = Iface_Const.CENTER;
-		WESTEAST = Iface_Const.CENTER;
-	}
-	
-	private void setPlaneCoords(int x, int y)
-	{
-		PLANE_COORD_X = x - Iface_Const.p_cx;
-		PLANE_COORD_Y= Iface_Const.INVERT*(y - Iface_Const.p_cy);
-	}
-	
-	public int calculateToFrom(int x, int y, int angle)
-	{
-		setPlaneCoords(x,y);
-		updateOutputs(angle);
-		return TOFROM;
-	}
-	
-	public int calculateWestEast(int x, int y, int angle)
-	{
-		setPlaneCoords(x,y);
-		updateOutputs(angle);
-		return WESTEAST;	
-	}
-	
-	private void updateOutputs(int angle)
-	{
-
-		double opp = PLANE_COORD_X;
-		double adj = PLANE_COORD_Y;
-		int plane_angle = (int)Math.toDegrees(Math.atan2(opp,adj));
-		int norm_angle = normalizedAngle(plane_angle);
-		
-		if(norm_angle == angle){
-			TOFROM = WESTEAST = Iface_Const.CENTER;
-			return;
-		} else {
-			if(norm_angle < ((angle + Iface_Const.HALF_PI)%
-					Iface_Const.TWO_PI)) {
-				TOFROM = Iface_Const.FROM;
-				WESTEAST = Iface_Const.EAST;
-			} else if (norm_angle < ((angle + Iface_Const.PI)%
-			Iface_Const.TWO_PI)) {
-				TOFROM = Iface_Const.TO;
-				WESTEAST = Iface_Const.EAST;
-			} else if (norm_angle < ((angle + 
-					Iface_Const.THREE_FORTH_PI)%
-					Iface_Const.TWO_PI)) {
-				TOFROM = Iface_Const.TO;
-				WESTEAST = Iface_Const.WEST;
-			} else if (norm_angle < ((angle + Iface_Const.TWO_PI)%
-					Iface_Const.TWO_PI)) {
-				TOFROM = Iface_Const.FROM;
-				WESTEAST = Iface_Const.WEST;
-			} else {
-				
-			}
-		}
 
 	}
+	
+//	public int calculateToFrom(int x, int y, int angle)
+//	{
+//		setPlaneCoords(x,y);
+//		updateOutputs(angle);
+//		return TOFROM;
+//	}
+//	
+//	public int calculateWestEast(int x, int y, int angle)
+//	{
+//		setPlaneCoords(x,y);
+//		updateOutputs(angle);
+//		return WESTEAST;	
+//	}
+	
+//	private void updateOutputs(int angle)
+//	{
+//
+//		double opp = PLANE_COORD_X;
+//		double adj = PLANE_COORD_Y;
+//		int plane_angle = (int)Math.toDegrees(Math.atan2(opp,adj));
+//		int norm_angle = normalizedAngle(plane_angle);
+//		
+//		if(norm_angle == angle){
+//			TOFROM = WESTEAST = Vor_Const.CENTER;
+//			return;
+//		} else {
+//			if(norm_angle < ((angle + Vor_Const.HALF_PI)%
+//					Vor_Const.TWO_PI)) {
+//				TOFROM = Vor_Const.FROM;
+//				WESTEAST = Vor_Const.EAST;
+//			} else if (norm_angle < ((angle + Vor_Const.PI)%
+//			Vor_Const.TWO_PI)) {
+//				TOFROM = Vor_Const.TO;
+//				WESTEAST = Vor_Const.EAST;
+//			} else if (norm_angle < ((angle + 
+//					Vor_Const.THREE_FORTH_PI)%
+//					Vor_Const.TWO_PI)) {
+//				TOFROM = Vor_Const.TO;
+//				WESTEAST = Vor_Const.WEST;
+//			} else if (norm_angle < ((angle + Vor_Const.TWO_PI)%
+//					Vor_Const.TWO_PI)) {
+//				TOFROM = Vor_Const.FROM;
+//				WESTEAST = Vor_Const.WEST;
+//			} else {
+//				
+//			}
+//		}
+//
+//	}
 
 	public int normalizedAngle(int angle) 
 	{
-		return ((angle + Iface_Const.TWO_PI)%
-			Iface_Const.TWO_PI);
+		return ((angle + Vor_Const.TWO_PI)%
+			Vor_Const.TWO_PI);
 	}
 	
 }
