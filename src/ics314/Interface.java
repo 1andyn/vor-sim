57208b257c864c82809ec841064c7fcb426d4520
andyng <andyng@hawaii.edu>
2014-04-11 20:14:06 -1000

    removed most old cold for previous implementation for interface

--- src/ics314/Interface.java
+++ src/ics314/Interface.java
@@ -47,53 +47,6 @@ public class Interface {
 		//Resource Setup
 		final List<Image> imagelist = new ArrayList<Image>();
 		icfg.LoadImages(Disp, imagelist);
-
-		//Column #1 VOR Background
-		Group Map_Disp = new Group(Disp_Shell, SWT.NONE);
-		Map_Disp.setText("VOR Map");
-		Map_Disp.setBackgroundMode(SWT.INHERIT_FORCE);
-		Map_Disp.setLayout(new FillLayout());
-		G_Data = new GridData(GridData.FILL, GridData.BEGINNING, true, false);
-		G_Data.widthHint = Interface_Const.BG_PIXELS;
-		G_Data.heightHint = Interface_Const.BG_PIXELS + 5;
-		Map_Disp.setLayoutData(G_Data);
-		
-		
-		final Image plane = imagelist.get(11);
-		final Rectangle plane_box = plane.getBounds();
-		final Image background = imagelist.get(0);
-		final Image line = imagelist.get(12);
-		final Rectangle line_box = line.getBounds();
-		final Canvas canvas = new Canvas(Map_Disp,SWT.NO_REDRAW_RESIZE |
-				SWT.DOUBLE_BUFFERED);
-	    canvas.addPaintListener(new PaintListener() {
-	        public void paintControl(PaintEvent e) {
-		        e.gc.drawImage(background, 0, 0);
-		        
-		        int x = vor_rad.getXCoord();
-		        int y = vor_rad.getYCoord();
-		        
-		        Transform transform = new Transform(Disp);
-		        transform.translate(x + plane_box.width/2, y + plane_box.height/2);
-		        transform.rotate(-vor_rad.getPlaneAngle());
-		        transform.translate(-(x + plane_box.width/2), -(y + plane_box.height/2));
-		        e.gc.setTransform(transform);
-		        e.gc.drawImage(plane, vor_rad.getXCoord(),vor_rad.getYCoord());
-		        transform.dispose();
-		        
-		        transform = new Transform(Disp);
-		        transform.translate(line_box.width/2, 8+line_box.height/2);
-		        transform.rotate(vor_rad.getOBSAngle()+ 
-		        		Interface_Const.VERTICAL_OFFSET);
-		        transform.translate(-(line_box.width/2), -(8 + line_box.height/2));
-		        e.gc.setTransform(transform);
-		        e.gc.drawImage(line, Interface_Const.LINE_X_CENTER, 
-		        		Interface_Const.LINE_Y_CENTER);
-		        transform.dispose();
-
-	        }
-	    });
-	    canvas.pack();
 		
 		//Secondary displays
 		Group Onboard_Disp = new Group(Disp_Shell, SWT.NONE);
@@ -121,14 +74,7 @@ public class Interface {
 
 		    	vor_rad.updateDisplays();
 		    	  
-		        Transform transform = new Transform(Disp);
-		        transform.translate(rect.width/2, rect.height/2);
-		        transform.rotate(vor_rad.getPlaneAngle());
-		        transform.translate(-rect.width/2, -rect.height/2);
-		        e.gc.setTransform(transform);
-		        e.gc.drawImage(compass, 0,0);
-		        transform.dispose();
-		        
+		        Transform transform = new Transform(Disp);		      
 		        transform = new Transform(Disp);
 		        transform.translate(0 + rect.width/2, (205 + rect.height/2));
 		        transform.rotate(-vor_rad.getOBSAngle());
@@ -137,16 +83,13 @@ public class Interface {
 		        e.gc.drawImage(obs, 0,205);
 		        transform.dispose();
 		     
-		        e.gc.setTransform(null);
-		        
-		        e.gc.drawImage(icfg.getWEImage(imagelist, vor_rad),115,300);
+		        e.gc.setTransform(null);		      
 		        e.gc.drawImage(icfg.getTFImage(imagelist, vor_rad),38, 320);
 		        
 		        
 		        /* Static Draws */
 		        e.gc.drawImage(obs_ptr, 93, 238);
-		        e.gc.drawImage(cmp_plane,85,80);
-		        
+        
 		      }
 		    });
 
@@ -158,40 +101,7 @@ public class Interface {
 		G_Data = new GridData(GridData.FILL, GridData.BEGINNING, true, false);
 		G_Data.horizontalSpan = Interface_Const.SUB_COL_COUNT;
 		G_Data.heightHint = 60;
-		Simulator_Inputs.setLayoutData(G_Data);
-		
-		/* X Coord Setup*/
-		Label PlaneCoordLabelX = new Label(Simulator_Inputs, SWT.SINGLE);
-		PlaneCoordLabelX.setText("Plane Coordinates X: ");	
-		PlaneCoordLabelX.pack();
-		PlaneCoordLabelX.setLocation(40, 25);
-		//Label Creations for User Interactables
-
-	    final Spinner PlaneX = new Spinner(Simulator_Inputs, SWT.READ_ONLY);
-	    icfg.SpinnerCoordConfig(PlaneX);
-	    PlaneX.pack();
-	    PlaneX.setLocation(170, 23);
-	    
-		Label PlaneCoordLabelY = new Label(Simulator_Inputs, 
-				SWT.SINGLE & SWT.BORDER);
-		PlaneCoordLabelY.setText("Plane Coordinates Y: ");
-		PlaneCoordLabelY.pack();
-		PlaneCoordLabelY.setLocation(40, 51);
-		
-		final Spinner PlaneY = new Spinner(Simulator_Inputs, SWT.READ_ONLY);
-	    icfg.SpinnerCoordConfig(PlaneY);
-	    PlaneY.pack();
-	    PlaneY.setLocation(170, 50);
-	    
-		Label PlaneAngleLabel = new Label(Simulator_Inputs, SWT.SINGLE);
-		PlaneAngleLabel.setText("Plane Angle (Degrees): ");
-		PlaneAngleLabel.pack();
-		PlaneAngleLabel.setLocation(330,25);
-	
-	    final Spinner PlaneAngle = new Spinner(Simulator_Inputs, SWT.READ_ONLY);
-	    icfg.SpinnerAngleConfig(PlaneAngle);
-		PlaneAngle.pack();
-		PlaneAngle.setLocation(510,23);
+		Simulator_Inputs.setLayoutData(G_Data);		
 	    
 		Label OBSLabel = new Label(Simulator_Inputs, SWT.SINGLE);
 		OBSLabel.setText("OBS Knob Angle (Degrees): ");
@@ -206,43 +116,6 @@ public class Interface {
 		Disp_Shell.pack();
 		Disp_Shell.open();
 		
-		PlaneX.addSelectionListener(new SelectionAdapter() {
-		      public void widgetSelected(SelectionEvent e) {
-		        int selection = PlaneX.getSelection();
-		        int digits = PlaneX.getDigits();
-		        int value = (int) (selection / Math.pow(10, digits));
-		        vor_rad.setXCoord(value + Interface_Const.p_cx);
-		        //System.out.println("PlaneX is " + value);
-		        canvas.redraw();
-		        cmp_cvs.redraw();
-		      }
-		    });
-		
-		PlaneY.addSelectionListener(new SelectionAdapter() {
-		      public void widgetSelected(SelectionEvent e) {
-		        int selection = PlaneY.getSelection();
-		        int digits = PlaneY.getDigits();
-		        int value = (int) (selection / Math.pow(10, digits));
-		        vor_rad.setYCoord(value*Interface_Const.INVERT + 
-		        		Interface_Const.p_cy);
-		        //System.out.println("PlaneY is " + value);
-		        canvas.redraw();
-		        cmp_cvs.redraw();
-		      }
-		    });
-		
-		PlaneAngle.addSelectionListener(new SelectionAdapter() {
-		      public void widgetSelected(SelectionEvent e) {
-		        int selection = PlaneAngle.getSelection();
-		        int digits = PlaneAngle.getDigits();
-		        int value = (int) (selection / Math.pow(10, digits));
-		        //System.out.println("Plane Angle is " + value);
-		        vor_rad.setPlaneAngle(value);
-		        cmp_cvs.redraw();
-		        canvas.redraw();
-		      }
-		    });
-		
 		OBSAngle.addSelectionListener(new SelectionAdapter() {
 		      public void widgetSelected(SelectionEvent e) {
 		        int selection = OBSAngle.getSelection();
@@ -251,7 +124,6 @@ public class Interface {
 		        //System.out.println("OBS Angle is " + value);
 		        vor_rad.setOBSAngle(value);
 		        cmp_cvs.redraw();
-		        canvas.redraw();
 		      }
 		    });
 		
